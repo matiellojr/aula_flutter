fc6fe4153fc847bde1e47ae73c3a1324a1025870
junior.matiello <junior.matiello@senior.com.br>
2026-04-07 11:06:12 -0300

    #AULA74 - Coluna e Textos

--- contador/lib/main.dart
+++ contador/lib/main.dart
@@ -9,11 +9,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        color: Colors.black,
-        alignment: Alignment.center,
-        child: const Text("Hello World"),
+    return MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text(
+            "Pode entrar!",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text('0', style: TextStyle(fontSize: 100, color: Colors.white)),
+        ],
       ),
     );
   }

40ebae05c62602430cd68cc25c5d48854f95bb1a
Junior.Matiello <matiello.junior@gmail.com>
2026-05-14 15:26:15 -0300

    #AULA88 - Personalizando o Campo de Texto

--- todo_list/lib/pages/todo_list_page.dart
+++ todo_list/lib/pages/todo_list_page.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
 
-
 class TodoListPage extends StatelessWidget {
   const TodoListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-      
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'Tarefa:',
+              hintText: 'Digite a tarefa a ser realizada',
+              // border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+              errorText: null,
+              prefixText: 'R\$ ',
+              suffixText: ',00', // ou cm, kg, etc
+              labelStyle: TextStyle(
+                fontSize: 30,
+                color: Colors.blue,
+              ),
+            ),
+            keyboardType: TextInputType.number,
+            // obscureText: true, // igual a senha ****
+            // obscuringCharacter: "*", // caracter para ocultar o texto
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
